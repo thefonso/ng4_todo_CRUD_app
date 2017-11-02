13a232de1babd542471897af67b1e314d857a679
thefonso <thefonso@thefonso.com>
2017-11-01 22:59:11 -0700

    first commit - basic crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import {Todo} from './todo';
+// Import class so we can register it as dependency injection token
+import {TodoDataService} from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app';
+
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  addTodo(){
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  // Service is now available as this.todoDataService
+  toggleTodoComplete(todo){
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo){
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
 }
