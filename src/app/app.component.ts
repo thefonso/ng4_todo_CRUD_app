dc8bcd1e4c2a74d6ca7757bcdf7c26b632cd88f2
thefonso <thefonso@thefonso.com>
2017-11-02 19:25:10 -0700

    Part 1 - complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Todo} from './todo';
 // Import class so we can register it as dependency injection token
 import {TodoDataService} from './todo-data.service';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
