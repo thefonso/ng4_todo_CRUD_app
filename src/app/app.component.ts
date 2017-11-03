6b7db270d43956b929db514e627e4235a5ff4e13
thefonso <thefonso@thefonso.com>
2017-11-02 20:32:46 -0700

    todo-list-header-component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import {Todo} from './todo';
-// Import class so we can register it as dependency injection token
 import {TodoDataService} from './todo-data.service';
 
 
@@ -14,18 +13,21 @@ export class AppComponent {
 
   newTodo: Todo = new Todo();
 
-  // Ask Angular Dependency Injections system to inject the dependency
-  // associated with the token `TodoDataService`
-  // and assign it to a property called `todoDataService`
+
   constructor(private todoDataService: TodoDataService) {
   }
 
-  addTodo(){
-    this.todoDataService.addTodo(this.newTodo);
-    this.newTodo = new Todo();
+  // No longer needed, now handled by TodoListHeaderComponent
+  // addTodo() {
+  //   this.todoDataService.addTodo(this.newTodo);
+  //   this.newTodo = new Todo();
+  // }
+
+  // Add new method to handle event emitted by TodoListHeaderComponent
+  onAddTodo(todo: Todo) {
+    this.todoDataService.addTodo(todo);
   }
 
-  // Service is now available as this.todoDataService
   toggleTodoComplete(todo){
     this.todoDataService.toggleTodoComplete(todo);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 import { AppComponent } from './app.component';
+import { TodoListHeaderComponent } from './todo-list-header/todo-list-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListHeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
