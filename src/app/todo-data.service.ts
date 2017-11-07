76ebb7b07328d64b9d6671a9526f61b6e8611796
thefonso <thefonso@thefonso.com>
2017-11-06 21:20:17 -0800

    Updating TodoDataService - becoming RESTful

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -1,59 +1,44 @@
 import { Injectable } from '@angular/core';
-import {Todo} from './todo';
+import { Todo } from './todo';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class TodoDataService {
 
-  lastId: number = 0;
-  todos: Todo[] = [];
-
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   //Simulate POST /todos
-  addTodo(todo: Todo): TodoDataService{
-    if (!todo.id) {
-      todo.id = ++this.lastId;
-    }
-    this.todos.push(todo);
-    return this;
+  addTodo(todo: Todo): Observable<Todo> {
+    return this.api.createTodo(todo);
   }
 
+
   // Simulate DELETE /todos/:id
-  deleteTodoById(id: number): TodoDataService{
-    this.todos = this.todos
-      .filter(todo  => todo.id !== id);
-    return this;
+  deleteTodoById(todoId: number): Observable<Todo> {
+    return this.api.deleteTodoById(todoId);
   }
 
 
   // Simulate PUT /todos/:id
-  updateTodoById(id: number, values: Object = {}): Todo{
-    let todo = this.getTodoById(id);
-    if (!todo) {
-      return null;
-    }
-    Object.assign(todo, values);
-    return todo;
+  updateTodo(todo: Todo): Observable<Todo>{
+    return this.api.updateTodo(todo);
   }
 
-  // Simulate GET /todos
-  getAllTodos(): Todo[] {
-    return this.todos;
+  // Simulate GET all /todos
+  getAllTodos(): Observable<Todo[]> {
+    return this.api.getAllTodos();
   }
 
-  // Simulate Get /todos/:id
-  getTodoById(id: number): Todo {
-    return this.todos
-      .filter(todo  => todo.id === id)
-      .pop();
+  // Simulate Get specific /todos/:id
+  getTodoById(todoId: number): Observable<Todo> {
+    return this.api.getTodoById(todoId);
   }
 
   // Toggle todo complete
   toggleTodoComplete(todo: Todo){
-    let updatedTodo = this.updateTodoById(todo.id, {
-      complete: !todo.complete
-    });
-    return updatedTodo;
+    todo.complete = !todo.complete;
+    return this.api.updateTodo(todo);
   }
 
 }
